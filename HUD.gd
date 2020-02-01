68e972210136bf56a9a944c37f438a0afbad4d21
VetoPlayer <andrewlloys@gmail.com>
2020-02-01 15:12:00 +0000

    [Adding whole scene structure]

--- HUD.gd
+++ HUD.gd
@@ -6,34 +6,21 @@ signal start_game
 func show_message(text):
 	$MessageLabel.text = text
 	$MessageLabel.show()
-	$MessageTimer.start()
 	
 func show_game_over():
 	## Shows game over for 2 seconds,
 	## Then return to the title screen and
 	## After a brief pause show the play button
 	show_message("Game Over!")
-	yield($MessageTimer, "timeout")
+	## TODO
+	##yield($MessageTimer, "timeout")
 	$MessageLabel.text = "Dodge the\nCreeps!"
 	$MessageLabel.show()
 	yield(get_tree().create_timer(1), 'timeout')
 	$StartButton.show()
 
 
-func update_score(score):
-	$ScoreLabel.text = str(score)
-	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_StartButton_pressed():
-	$StartButton.hide()
-	emit_signal("start_game")
+	get_tree().change_scene("res://choose.tscn")
 
-
-func _on_MessageTimer_timeout():
-	$MessageLabel.hide()
--- Main.gd
+++ Main.gd
@@ -6,55 +6,28 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	randomize()
+	$Player1.start($StartPosition1.position, true)
+	$Player2.start($StartPosition2.position, false)
+	$HUD.show_message("Get Ready!")
+	$Music.play()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 
 func _on_Player_hit(is_first_player, damage):
 	## When the player is hit, the game is over
 	print("I see the player being hit")
 	print(damage)
-	$ScoreTimer.stop()
-	$MobTimer.stop()
-	$HUD.show_game_over()
+	##$HUD.show_game_over()
 	$DeathSound.play()
 	$Music.stop()
 	
 	
-func new_game():
-	score = 0
-	$Player1.start($StartPosition1.position, true)
-	$Player2.start($StartPosition2.position, false)
-	$HUD.update_score(score)
-	$HUD.show_message("Get Ready")
-	$Music.play()
 
 
-func _on_ScoreTimer_timeout():
-	score += 1
-	$HUD.update_score(score)
-
-
-
-func _on_MobTimer_timeout():
-	 #Choose a random location
-	$MobPath/MobSpawnLocation.set_offset(randi())
-	# Create a mob instance and add it to the scene
-	var mob = Mob.instance()
-	add_child(mob)
-	# Set the mob direction perpendicular to the path direction
-	var direction = $MobPath/MobSpawnLocation.rotation + PI/2
-	direction += rand_range(-PI/4, PI/4)
-	mob.rotation = direction
-	mob.position = $MobPath/MobSpawnLocation.position
-	## Set Linear velocity
-	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed),0)
-	mob.linear_velocity = mob.linear_velocity.rotated(direction)
-
+func new_player_selection():
+	get_tree().change_scene("res://choose.tscn")
 
 
 func _on_HUD_start_game():
-	new_game()
+	## This one afterwards will need to be performed if it's the first time
+	new_player_selection()
