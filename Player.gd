4e9f69c8289f7355db0a00427a545d4f1db04fad
VetoPlayer <andrewlloys@gmail.com>
2020-02-01 12:59:11 +0000

    [Adding Head, Torso and Legs]

--- Player.gd
+++ Player.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 var first_player = false
+## TODO: You do want to flip the player sprite if it's now the first player
 signal hit(is_first_player, damage)
 export var speed = 400 # pixel/sec - How fast the player will move.
 var screen_size # Size of the game window
@@ -30,29 +31,23 @@ func _process(delta):
 		velocity = velocity.normalized() * speed
 		## Play the actual animation according to the velocity
 		if velocity.x != 0:
-			$AnimatedSprite.animation = "walk"
-			$AnimatedSprite.flip_v = false
-			$AnimatedSprite.flip_h = velocity.x < 0
+			$Torso.animation = "walk"
+			$Torso.flip_v = false
+			$Torso.flip_h = velocity.x < 0
 		if velocity.y != 0:
-			$AnimatedSprite.animation = "up"
-			$AnimatedSprite.flip_v = velocity.y > 0
-		$AnimatedSprite.play()
+			$Torso.animation = "up"
+			$Torso.flip_v = velocity.y > 0
+		$Torso.play()
 	else:
-		$AnimatedSprite.stop()
+		$Torso.stop()
 	# Actually update the player position
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 
-func _on_Player_body_entered(body):
-	## Player disappears after being hit
-	hide()
-	emit_signal("hit")
-	## set_deferred: Disable collision shape only when it's safe to do so.
-	## We actually want this to be disabled because we don't want to happen twice
-	$CollisionShape2D.set_deferred("disabled", true)
 
 func start(pos, is_first_player):
+	## TODO: Add head, torso and legs type
 	## When the game starts, move the player in the given position and reveal it.
 	position = pos
 	if is_first_player:
@@ -66,4 +61,8 @@ func start(pos, is_first_player):
 func _on_Player_area_entered(area):
 	print("I have been hit, emitting signal")
 	## TODO: You should asks to the area2D just entered how much the damage is.
+	## TODO: You do want to check out your current status, i.e. whether you are blocking
 	emit_signal("hit", first_player, 10)
+	## set_deferred: Disable collision shape only when it's safe to do so.
+	## We actually want this to be disabled because we don't want to happen twice
+	##$CollisionShape2D.set_deferred("disabled", true)
